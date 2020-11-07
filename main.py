d0e160f47c941e086a820bcf8482d36307e5e412
Clanz99 <67827361+Clanz99@users.noreply.github.com>
2020-11-07 18:43:05 +0100

    Added getFollowing

--- main.py
+++ main.py
@@ -36,8 +36,37 @@ def getFollowers(account, path):
 
     serialize_json(data_folder, path, followers_of_account)
 
+def getFollowing(account, path):
+    followings_of_account = []
+
+    for item in tweepy.Cursor(
+            api.friends,
+            screen_name=account,
+            skip_status=True,
+            include_user_entities=False
+    ).items():
+
+        json_data = item._json
+
+        found_following = {}
+        found_following["id"] = json_data["id"]
+        found_following["name"] = json_data["name"]
+        found_following["screen_name"] = json_data["screen_name"]
+        found_following["location"] = json_data["location"]
+        if found_following not in followings_of_account:
+            followings_of_account.append(found_following)
+        print(f"Processing Following {len(followings_of_account)} of account: {account}")
+
+    serialize_json(data_folder, path, followings_of_account)
+
 #getFollowers("mizzaro", "mizzaroFollowers.json")
 #getFollowers("damiano10", "damiano10Followers.json")
 #getFollowers("Miccighel_", "Miccighel_Followers.json")
 #getFollowers("eglu81", "eglu81Followers.json")
-getFollowers("KevinRoitero", "KevinRoiteroFollowers.json")
+#getFollowers("KevinRoitero", "KevinRoiteroFollowers.json")
+
+#getFollowing("mizzaro", "mizzaroFollowers.json")
+#getFollowing("damiano10", "damiano10Followers.json")
+#getFollowing("Miccighel_", "Miccighel_Followers.json")
+#getFollowing("eglu81", "eglu81Followers.json")
+getFollowing("KevinRoitero", "KevinRoiteroFollowings.json")
